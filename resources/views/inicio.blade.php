dd410b0d9f79fb9580817843a233b71fe024122e
salvi <salolinu@gmail.com>
2021-02-18 18:23:13 +0100

    Añadido el numero de top

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -29,28 +29,23 @@
         </div>
         <div class="col-6">
         <h3 class="text-center my-3">Top 10 peliculas</h3>
+        @php ($id = 1)
         @foreach ($top10 as $peliculatop)
         <div class="card mx-auto my-3" style="width:100%">
             <div class="row">
                 <div class="col-1">
-                    <h3 class="text-center px-3" id="number">1</h3>
+                    <h3 class="text-center px-3" id="number">{{ $id }}</h3>
                 </div>
                 <div class="col-3">
                     <img class="card-img-top img-fluid" src="{{ $peliculatop->imagen}}" alt="Card image">
                 </div>
                 <div class="card-body col-8">
                     <h4 class="card-title">{{ $peliculatop->titulo }}</h4>
-                    <p class="card-text">
-                        <ul>
-                            <li>Genero: {{ $peliculatop->genero }}</li>
-                            <li>Productora: {{ $peliculatop->productora }}</li>
-                            <li>Nota: {{ $peliculatop->nota }}</li>
-                        </ul>
-                    </p>
                     <a href="#" class="btn btn-primary">Ver detalles</a>
                 </div>
             </div>
         </div>
+        @php ($id++)
         @endforeach
 
     </div>
